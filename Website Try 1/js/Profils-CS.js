2f7b4a0fe005b900a952e90deb49a0143ac70507
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-04-18 19:49:49 +0200

    Update Profils-CS.js

--- Website Try 1/js/Profils-CS.js
+++ Website Try 1/js/Profils-CS.js
@@ -1,40 +1,38 @@
-let personne = [
-{nom : 'Beauthier', prenom : 'Lisa', age : 23, sexe : 'F', etoile : '★☆☆☆☆', description : "J'aime courir partout en criant houlala", photo : "lisbonne.png"},
-{nom : 'Kikou', prenom : 'Tom', age : 12, sexe : 'M', etoile : '★★★★★', description : "J'aime être en confinement et ne rien faire du tout!", photo : "rio.jpg"},
-{nom : 'Abracadabra', prenom : 'Alban', age : 45, sexe : 'M', etoile : '★★★★☆', description :"Aller chercher du chocolat au magasin, c'est ma passion", photo : "professeur.jpg"},
-{nom : 'Akina', prenom : 'Amélie', age : 27, sexe : 'F', etoile : '★★★☆☆', description : "Je tonds la pelouse aussi vite que flash mcqueen", photo : "tokyo.jpg"},
-{nom : 'Bibou', prenom : 'Laura', age : 34, sexe : 'F', etoile : '★★★☆☆', description : "J'aime les pâtes", photo : "alicia.jpg"},
-{nom : 'Boh', prenom : 'Aladin', age : 13, sexe : 'M', etoile : '★★★☆☆', description : "Qui pisse contre le vent se rince les dents", photo : "berlin.jpg"},
-{nom : 'Ahah', prenom : 'Robin', age : 98, sexe : 'M', etoile : '★★★☆☆', description : "I travelled the world and the seven seas, everybody's looking for something", photo : "bogota.jpg"},
-{nom : 'Loulo', prenom : 'Mireille', age : 30, sexe : 'F', etoile : '★★★☆☆', description : "Il en faut peu pour être heureux", photo : "palerme.jpg"},
-{nom : 'Mimi', prenom : 'Martine', age : 67, sexe : 'F', etoile : '★★★☆☆', description : "Hasta la vista", photo : "nairobi.jpg"},
-{nom : 'Narci', prenom : 'Eliot', age : 50, sexe : 'M', etoile : '★★★☆☆', description : "Je suis à la cuisine ce que Mozart est à la musique classique", photo : "helsinki.jpg"}
-]
-
 function demarrage() {
+	let xhr = new XMLHttpRequest() ;
+	xhr.open('get', "http://localhost/json?url=Profils-CSS", true) ; 
+	xhr.onreadystatechange = 
+			function affichage() {
+				if (xhr.status == 200 && xhr.readyState == 4) {
+						document.getElementById('principal').innerHTML = affiche(xhr.responseText) ;
+					}
+			}
+	xhr.send() ;
+}
+
+
+function affiche(texte) {
 	
-	let fiche = "", img ;
-	for(let i in personne) {
-		img = '<img src="img/'+ personne[i].photo +'" alt="test" width="234" height="250"/>' ;
-		fiche += "<div class='personne'>\
-		             <div class='avatars'> " + img + "</div>\
-					 <div class='desc'>\
-					 <div class='nom_com'>" + personne[i].prenom + "     " + personne[i].etoile + "</div>\
-					 <div class='info'><br><br><br><br>" + personne[i].description + "</div>\
-			         <div id='profil_bouton'><input type='button' value='Voir le profil' class='compte_bouton'></div>\
-					 </div></div>" ;
+	let reponse = JSON.parse(texte) ;
+	let fiche = "", img, desc ;
+	for (let i = 0 ; i < reponse.length ; i++) {
+		img = '<img src="./img/'+ reponse[i].photo +'" alt="test" width="234" height="250"/>' ;
+		if (reponse[i]['portrait'] == null) {
+			desc = "Pas de description" ;
+		}
+		else {
+			desc = reponse[i].portrait ;
+		}
+			fiche += "<div class='personne'>\
+						 <div class='avatars'> " + img + "</div>\
+						 <div class='desc'>\
+						 <div class='nom_com'>" + reponse[i].prenom + "     " + '★★★★☆' + "</div>\
+						 <div class='info'><br><br><br><br>" + desc + "</div>\
+						 <div id='profil_bouton'><a href='PageProfilPublic-CB'><input type='button' value='Voir le profil' class='compte_bouton'></a></div>\
+						 </div></div>" ;
 	}
-	document.getElementById("principal").innerHTML = fiche ;	
+	return fiche ;
 }
-
-function test() {
-	
-}
-
-
-
-
-
 
 
 
