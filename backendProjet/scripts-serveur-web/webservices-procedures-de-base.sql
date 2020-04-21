2b4024cbb124287a32f7ee98db55c5ca156c6c9e
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-04-21 19:21:39 +0200

    Create webservices-procedures-de-base.sql

---
+++ backendProjet/scripts-serveur-web/webservices-procedures-de-base.sql
@@ -0,0 +1,44 @@
+_______________________-Chemin de la base de données-________________________________
+ALTER FUNCTION "DBA"."getPath"() 
+returns long varchar deterministic 
+BEGIN
+    DECLARE dbPath long varchar; 
+    DECLARE dbName long varchar;
+    SET dbPath = (select db_property ('file'));
+    SET dbName = (select db_property('name')) + '.db'; 
+    SET dbPath = left(dbPath, length(dbPath)-length(dbName)); 
+    RETURN dbPath ; 
+END
+_______________________-CSS-________________________________
+
+ALTER PROCEDURE "DBA"."http_getCSS"(in url char(255)) 
+RESULT(css long varchar)
+BEGIN
+    CALL sa_set_http_header( 'Content-Type', 'text/css' );
+    select xp_read_file(dba.getPath() || 'CSS\'|| url) ; 
+END
+_______________________-Images-________________________________
+
+ALTER PROCEDURE "DBA"."http_getIMG"(in filename char(255)) 
+RESULT(img long varchar)
+BEGIN
+    CALL sa_set_http_header( 'Content-Type', 'image/png' );
+    select xp_read_file(dba.getPath() || 'img\'|| filename) ; 
+END
+_______________________-JavaScript-________________________________
+
+ALTER PROCEDURE "DBA"."http_getJS"(in url char(255)) 
+RESULT(js long varchar)
+BEGIN
+    CALL sa_set_http_header( 'Content-Type', 'text/js' );
+    select xp_read_file(dba.getPath() || 'JS\'|| url) ; 
+END
+_______________________-HTML-________________________________
+
+ALTER PROCEDURE "DBA"."http_getPage"(in url char(255)) 
+RESULT(html long varchar)
+BEGIN
+    CALL sa_set_http_header( 'Content-Type', 'text/html' );
+    Call sa_set_http_header('Access-Control-Allow-Origin', '*');
+    select xp_read_file(dba.getPath() || url || '.html'); 
+END
