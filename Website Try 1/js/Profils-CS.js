76370d49e8387da92c5e9cedc60b733096f99bf9
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-04-24 16:59:32 +0200

    Update Profils-CS.js

--- Website Try 1/js/Profils-CS.js
+++ Website Try 1/js/Profils-CS.js
@@ -1,6 +1,7 @@
+
 function demarrage() {
 	let xhr = new XMLHttpRequest() ;
-	xhr.open('get', "http://localhost/json?url=Profils-CSS", true) ; 
+	xhr.open('get', "http://localhost/json", true) ; 
 	xhr.onreadystatechange = 
 			function affichage() {
 				if (xhr.status == 200 && xhr.readyState == 4) {
@@ -23,19 +24,28 @@ function affiche(texte) {
 		else {
 			desc = reponse[i].portrait ;
 		}
-			fiche += "<div class='personne'>\
-						 <div class='avatars'> " + img + "</div>\
-						 <div class='desc'>\
-						 <div class='nom_com'>" + reponse[i].prenom + "     " + '★★★★☆' + "</div>\
-						 <div class='info'><br><br><br><br>" + desc + "</div>\
-						 <div id='profil_bouton'><a href='PageProfilPublic-CB'><input type='button' value='Voir le profil' class='compte_bouton'></a></div>\
-						 </div></div>" ;
+			fiche += '<div class="personne">\
+						 <div class="avatars"> ' + img + '</div>\
+						 <div class="desc">\
+						 <div class="nom_com">' + reponse[i].prenom + "     " + "★★★★☆" + '</div>\
+						 <div class="info"><br><br><br><br>' + desc + '</div>\
+						 <div id="profil_bouton"><a href="root?url=PageProfilPublic-CB"><input onclick="blabla(\'' + reponse[i].idCom + '\');" type="button" value="Voir le profil" class="compte_bouton"></a></div>\
+						 </div></div>' ;
 	}
 	return fiche ;
 }
 
-
-
+function blabla(code) {
+	let xhr = new XMLHttpRequest() ;
+	xhr.open('get', "http://localhost/stockages?identifiant=" + code , true) ; 
+	xhr.onreadystatechange = 
+			function affichage() {
+				if (xhr.status == 200 && xhr.readyState == 4) {
+						document.getElementById('principal').innerHTML = xhr.responseText ;
+					}
+			}
+	xhr.send() ;
+}
 
 
 
