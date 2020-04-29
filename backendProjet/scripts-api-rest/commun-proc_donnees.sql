e4939223ceaef44c6f19cc33857c37b217725bd7
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-04-29 15:32:38 +0200

    Update commun-proc_donnees.sql

--- backendProjet/scripts-api-rest/commun-proc_donnees.sql
+++ backendProjet/scripts-api-rest/commun-proc_donnees.sql
@@ -1,12 +1,14 @@
 //Pour la page Profils-CS.html
-CREATE PROCEDURE "DBA"."donnees"() 
+CREATE PROCEDURE proc_donnees() 
 RESULT (idCom VARCHAR, nom VARCHAR, prenom VARCHAR, sexe CHAR(1), telephone VARCHAR, mail VARCHAR, anniversaire DATE, photo VARCHAR, portrait VARCHAR)
-BEGIN 
-    
+BEGIN
     call sa_set_http_header('Access-Control-Allow-Origin', '*');
     select idCom, nom, prenom, sexe, telephone, mail, anniversaire, photo, portrait
     from dba.communaute ;
+END ;
 
-END
 
 
+
+//Service qui appelle donnees
+CREATE SERVICE "serv_donnees TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_donnees();
