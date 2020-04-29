4261404cf2c61a3d141e62c8a03f747d1470f7ea
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-04-29 15:55:11 +0200

    Update insertion-donnees.sql

--- backendProjet/scripts-base-de-donnees/insertion-donnees.sql
+++ backendProjet/scripts-base-de-donnees/insertion-donnees.sql
@@ -27,7 +27,7 @@ VALUES
 ('lou15', '9', 'County', 'Lou', 'M', '+32475655768', 'county.lou@gmail.com', '1980-05-22', 'lou.jpg', 'J''adore voyager, j''ai visité la plupart des continents. Ces vacances-ci j''opte pour l''Océanie !', 4000),
 ('jade35', '10', 'Wheeler', 'Jade', 'F', '+32488675908', 'wheeler.jade@gmail.com', '1985-08-02', 'fantome_rose.png', NULL, 5000),
 ('clemence75', '11', 'Powell', 'Clémence', 'F', '+32455463758', 'powell.clemence@gmail.com', '1986-07-26', 'fantome_mauve.png', NULL, 1348),
-('aubin87', '12', 'Long', 'Aubin', 'M', '+32400989075', 'long.aubin@gmail.com', '1989-07-30', 'aunbin.jpg', 'Je suis cycliste professionel, j''ai fais 3 fois le tour de France et j''adore ça.', 1000),
+('aubin87', '12', 'Long', 'Aubin', 'M', '+32400989075', 'long.aubin@gmail.com', '1989-07-30', 'aubin.jpg', 'Je suis cycliste professionel, j''ai fais 3 fois le tour de France et j''adore ça.', 1000),
 ('colin23', '13', 'Palmer', 'Colin', 'M', '+32409765665', 'palmer.colin@gmail.com', '1995-06-30', 'colin.jpg', 'Je suis passé à The Voice l''année passée car j''adore chanter.', 2000),
 ('elsa86', '14', 'Cox', 'Elsa', 'F', '+32422341198', 'coxe.elsa@gmail.com', '1992-10-28', 'fantome_mauve.png', 'Je fais de la boxe ainsi que de la danse classique, je cherche l''équilibre parfait.', 3500),
 ('adele98', '15', 'Ward', 'Adèle', 'F', '+32490908754', 'ward.adele@gmail.com', '1992-11-29', 'adele.jpg', 'Je suis très gourmande et je suis maniaque du rangement.', 1000),
