b4a1de0590d1d1ce6dc885b943edabe002205b0c
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-04-29 17:15:26 +0200

    Update Profils-CS.js

--- Website Try 1/js/Profils-CS.js
+++ Website Try 1/js/Profils-CS.js
@@ -17,7 +17,7 @@ function affiche(texte) {
 	let fiche = "", img, desc, cotation ;
 	let plein, moitiePlein, pasPlein ;
 	for (let i = 0 ; i < reponse.length ; i++) {
-		img = '<img src="./img/'+ reponse[i].photo +'" alt="test" width="234" height="250"/>' ;
+		img = '<img src="img?url='+ reponse[i].photo +'" alt="test" width="234" height="250"/>' ;
 		if (reponse[i]['portrait'] == null) {
 			desc = "Pas de description" ;
 		}
@@ -37,23 +37,24 @@ function affiche(texte) {
 					 <div class="desc">\
 					 <div class="nom_com"><span id="prenom_personne">' + reponse[i].prenom + '</span>' + cotation + '</span></div>\
 					 <div class="info"><br><br><br><br>' + desc + '</div>\
-					 <div id="profil_bouton"><a href="root?url=PageProfilPublic-CB"><input onclick="lien(\'' + reponse[i].idCom + '\');" type="button" value="Voir le profil" class="compte_bouton"></a></div>\
+					 <div id="profil_bouton"><a href="root?url=PageProfilPublic-CB"><input onclick="blabla(\'' + reponse[i].idCom + '\');" type="button" value="Voir le profil" class="compte_bouton"></a></div>\
 					 </div></div>' ;
 	}
 	return fiche ;
 }
 
-function lien(code) {
+function blabla(code) {
 	let xhr = new XMLHttpRequest() ;
 	xhr.open('get', "http://localhost/stockages?identifiant=" + code, true) ; 
 	xhr.onreadystatechange = 
 			function affichage() {
 				if (xhr.status == 200 && xhr.readyState == 4) {
-						document.getElementById('principal').innerHTML = xhr.responseText ;
+						xhr.responseText ;
 					}
 			}
 	xhr.send() ;
 }
+
 
 
 
