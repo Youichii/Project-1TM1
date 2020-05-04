8e94e839389647719e5cae7b8bbbdb3c4e461f8d
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2020-05-04 16:26:41 +0200

    Update cecile-proc_commentaire.sql

--- backendProjet/scripts-api-rest/Cecile/cecile-proc_commentaire.sql
+++ backendProjet/scripts-api-rest/Cecile/cecile-proc_commentaire.sql
@@ -12,10 +12,3 @@ VALUES
 END
 
 
-create service "serv_com"
-TYPE 'JSON' 
-       AUTHORIZATION OFF 
-       USER "DBA" 
-       URL ON 
-       METHODS 'GET' 
-AS call proc_com(:new_comm,:new_auteur,:utilisateur)
