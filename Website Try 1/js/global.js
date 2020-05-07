7dd1a7ddcf950a3c2a740b04dd265fe754b54c70
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-07 18:01:51 +0200

    Create global.js

---
+++ Website Try 1/js/global.js
@@ -0,0 +1,24 @@
+function start(ident) {
+	cacher(ident) ;
+}
+
+function cacher(identifiant) {
+	
+	document.getElementById("accueil_ab").style.display = "none" ;
+	document.getElementById("affiner_nk").style.display = "none" ;
+	document.getElementById("ajouter_nk").style.display = "none" ;
+	document.getElementById("loginpage_ab").style.display = "none" ;
+	document.getElementById("pageprofilprive_cb").style.display = "none" ;
+	document.getElementById("pageprofilpublic_cb").style.display = "none" ;
+	document.getElementById("profils_cs").style.display = "none" ;
+	document.getElementById("signuppage_ab").style.display = "none" ;
+	document.getElementById("taches_nk").style.display = "none" ;
+	
+	document.getElementById(identifiant).style.display = "inline" ;
+}
+
+function compte(utilisateurEnCours) {
+	console.log(utilisateurEnCours) ;
+	document.getElementById("tout").id = utilisateurEnCours ;
+}
+
