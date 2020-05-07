6706db75462e2790d4f0383d9334f15eb6e78bc6
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-07 18:47:06 +0200

    Update aurelien_brille-webservice-receiveData.sql

--- backendProjet/scripts-api-rest/aurelien_brille-webservice-receiveData.sql
+++ backendProjet/scripts-api-rest/aurelien_brille-webservice-receiveData.sql
@@ -2,7 +2,6 @@
 
 
 CREATE PROCEDURE "DBA"."inscription"(IN LastName long varchar, IN FirstName long VARCHAR, IN Username long varchar, IN uMail long varchar, IN uPhone long varchar, IN uVille long varchar, in uBirth DATE , in uPswd long varchar, in uSex long varchar, in Color long varchar)
-
 BEGIN 
     call sa_set_http_header('Access-Control-Allow-Origin', '*');
     INSERT INTO dba.communaute (idCom, mdp, nom, prenom , sexe, telephone, mail, anniversaire, photo, postal)
