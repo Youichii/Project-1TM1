46260ffeefb359aa536168f806e719381202b455
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-07 18:40:54 +0200

    Create cecile_bonnet-webservice-serv_Mail.sql

---
+++ backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_Mail.sql
@@ -0,0 +1,15 @@
+/* Auteur : Cécile Bonnet matricule */
+
+
+CREATE PROCEDURE proc_Mail(in new_mail VARCHAR(20),new_utilisateur VARCHAR(50))
+BEGIN 
+  update dba.communaute
+  set mail  = new_mail
+  where idCom = new_utilisateur 
+END
+
+
+/******************************************************/
+
+
+CREATE SERVICE "serv_Mail" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_Mail(:new_mail,:new_utilisateur);
