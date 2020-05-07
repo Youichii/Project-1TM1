ae9ece275e13e3ea6499c9fbdfd6b18f0083a8d9
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-07 18:42:06 +0200

    Create cecile_bonnet-webservice-serv_Note.sql

---
+++ backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_Note.sql
@@ -0,0 +1,18 @@
+/* Auteur : Cécile Bonnet matricule */
+
+
+CREATE PROCEDURE proc_Note(in new_note INTEGER,new_utilisateur VARCHAR(50))
+
+BEGIN 
+
+update dba.taches
+set note  = new_note
+where idCom = new_utilisateur 
+
+END
+
+
+/******************************************************/
+
+
+CREATE SERVICE "serv_Note" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_Note(:new_note,:utilisateur);
