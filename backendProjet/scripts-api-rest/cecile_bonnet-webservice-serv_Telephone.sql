53efe3b5d2b7d5ecbd93e993a465cbea652779d3
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-07 18:43:20 +0200

    Create cecile_bonnet-webservice-serv_Telephone.sql

---
+++ backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_Telephone.sql
@@ -0,0 +1,15 @@
+/* Auteur : Cécile Bonnet matricule */
+
+
+CREATE PROCEDURE proc_Telephone(in new_telephone VARCHAR(12),new_utilisateur VARCHAR(50))
+BEGIN 
+  update dba.communaute
+  set telephone = new_telephone 
+  where idCom = new_utilisateur 
+END
+
+
+/******************************************************/
+
+
+CREATE SERVICE "serv_Telephone" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_Telephone(:new_telephone,:new_utilisateur);
