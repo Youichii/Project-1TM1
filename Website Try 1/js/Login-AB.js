5b08f6864d0011705c78670ec0bfb9a0d856733d
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-07 18:02:26 +0200

    Update Login-AB.js

--- Website Try 1/js/Login-AB.js
+++ Website Try 1/js/Login-AB.js
@@ -1,18 +1,22 @@
-let profilConnexion ="";
+let profilConnexion = "";
 
 function changePage(){
-  let xhr = new XMLHttpRequest() ;
-	xhr.open('get', "http://localhost/serv_connexion?nomUtilisateur=" + document.getElementById("nomUtilisateur").value + "&motDePasse=" + document.getElementById("motDePasse").value , true) ;
-  xhr.onreadystatechange =
+	let xhr = new XMLHttpRequest() ;
+	let nom = document.getElementById("nomUtilisateur").value
+	xhr.open('get', "http://localhost/serv_connexion?nomUtilisateur=" + nom + "&motDePasse=" + document.getElementById("motDePasse").value , true) ;
+	xhr.onreadystatechange =
 			function affichage() {
 				if (xhr.status == 200 && xhr.readyState == 4) {
 					 profilConnexion = JSON.parse(xhr.responseText) ;
-           if(profilConnexion.length){
-              document.getElementById("blocCo").innerHTML = "<h1>Connexion Réussie!!</h1></br><a href='root?url=PageProfilPrive-CB'>Mon profil privé</a>";
-            }
-            else alert("Mauvais identifiants, réésayez ou inscrivez-vous");
+					if(profilConnexion.length){
+						document.getElementById("blocCo").innerHTML = "<h1>Connexion Réussie!!</h1></br><a onclick='cacher(\"pageprofilprive_cb\"), pageChargementBis(document.body.id);'>Mon profil privé</a>";
+						compte(nom) ;
+					}
+					else alert("Mauvais identifiants, réésayez ou inscrivez-vous");
 				}
 			}
   xhr.send();
   return false;
 }
+
+
