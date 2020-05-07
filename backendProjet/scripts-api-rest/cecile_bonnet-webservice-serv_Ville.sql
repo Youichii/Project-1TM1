82feceaed57897195da06a7077c297a0b1ec76eb
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-07 18:46:15 +0200

    Create cecile_bonnet-webservice-serv_Ville.sql

---
+++ backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_Ville.sql
@@ -0,0 +1,15 @@
+/* Auteur : Cécile Bonnet matricule */
+
+
+CREATE PROCEDURE proc_Ville(in new_ville VARCHAR(50),new_utilisateur VARCHAR(50))
+BEGIN 
+  update dba.communaute
+  set ville  = new_ville
+  where idCom = new_utilisateur 
+END
+
+
+/******************************************************/
+
+
+CREATE SERVICE "serv_Ville" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_Ville(:new_ville,:new_utilisateur);
