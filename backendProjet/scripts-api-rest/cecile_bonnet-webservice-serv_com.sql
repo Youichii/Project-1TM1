f4762646be0f4b52f954824c4ee445645d24a8a9
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-07 18:37:38 +0200

    Create cecile_bonnet-webservice-serv_com.sql

---
+++ backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_com.sql
@@ -0,0 +1,16 @@
+/* Auteur : Cécile Bonnet matricule */
+
+
+CREATE PROCEDURE "DBA"."proc_Com"(in utilisateur VARCHAR(25), new_com LONG VARCHAR, new_auteur VARCHAR(25))
+BEGIN 
+insert into dba.commentaires
+(idCom,commentaire,auteur)
+VALUES 
+(utilisateur,new_com,new_auteur);
+END
+
+
+/******************************************************/
+
+
+CREATE SERVICE "serv_com" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_com(:new_comm,:new_auteur,:utilisateur) ;
