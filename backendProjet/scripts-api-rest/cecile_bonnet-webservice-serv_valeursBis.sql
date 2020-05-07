26c07bfad00b3378fe228551d1625ac48ada3938
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-07 18:50:32 +0200

    Create cecile_bonnet-webservice-serv_valeursBis.sql

---
+++ backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_valeursBis.sql
@@ -0,0 +1,21 @@
+/* Auteur : Cécile Bonnet matricule */
+
+
+CREATE PROCEDURE "DBA"."proc_valeursBis"(in usager VARCHAR(16)) 
+RESULT (idCom VARCHAR, nom VARCHAR, prenom VARCHAR, sexe CHAR(1), telephone VARCHAR, mail VARCHAR, anniversaire DATE, photo VARCHAR, portrait VARCHAR, 
+        tache VARCHAR, commentaire VARCHAR, auteur VARCHAR, cote VARCHAR, ville VARCHAR)
+BEGIN 
+    Call sa_set_http_header('Access-Control-Allow-Origin', '*');
+    select commu.idCom, nom, prenom, sexe, telephone, mail, anniversaire, photo, portrait, tache, commentaire, auteur, cote, ville
+    from dba.communaute as commu
+    left join dba.taches as tach on commu.idCom = tach.idCom
+    left join dba.commentaires as comme on commu.idCom = comme.idCom
+    left join dba.villes as vill on commu.postal = vill.postal 
+    where commu.idCom = usager 
+END
+
+
+/******************************************************/
+
+
+CREATE SERVICE "serv_valeursBis" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_valeursBis(:usager);
