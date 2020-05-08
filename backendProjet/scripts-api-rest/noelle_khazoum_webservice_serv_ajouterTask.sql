a4c3be0437bf4e289cadcb12a189adf1a853898b
noellekh <63249943+noellekh@users.noreply.github.com>
2020-05-08 11:00:21 +0200

    Create noelle_khazoum_webservice_serv_ajouterTask.sql

---
+++ backendProjet/scripts-api-rest/noelle_khazoum_webservice_serv_ajouterTask.sql
@@ -0,0 +1,19 @@
+// pour ajouter des taches à la base de donnée
+
+CREATE PROCEDURE proc_ajouterTask(in task_ VARCHAR (50), in personne_ VARCHAR, in desc_ LONG VARCHAR, in cat_ VARCHAR)
+BEGIN
+     insert into dba.taches
+     (idCom, tache, details, categorie)
+     VALUES
+     (task_,personne_,desc_,cat_,)
+END
+
+
+
+CREATE SERVICE "serv_ajouterTask"
+TYPE 'JSON'
+      AUTHOTIZATION OFF
+      USER "DBA"
+      URL ON
+      METHODS 'GET'
+AS call proc_ajouterTask(:task_:,personne_,:desc_,;cat_)
