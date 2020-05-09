6250b154449530ac80869e20e9f49a8f3fd5053a
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-09 16:32:40 +0200

    Update clementine_sacre-webservice-serv_donnees.sql

--- backendProjet/scripts-api-rest/clementine_sacre-webservice-serv_donnees.sql
+++ backendProjet/scripts-api-rest/clementine_sacre-webservice-serv_donnees.sql
@@ -2,12 +2,13 @@
 
 
 CREATE PROCEDURE proc_donnees() 
-RESULT (idCom VARCHAR, nom VARCHAR, prenom VARCHAR, sexe CHAR(1), telephone VARCHAR, mail VARCHAR, anniversaire DATE, photo VARCHAR, portrait VARCHAR)
-BEGIN
-    call sa_set_http_header('Access-Control-Allow-Origin', '*');
-    select idCom, nom, prenom, sexe, telephone, mail, anniversaire, photo, portrait
-    from dba.communaute ;
-END ;
+RESULT (idCom VARCHAR(16), nom VARCHAR(30), prenom VARCHAR(30), photo VARCHAR(50), portrait VARCHAR(120), cote INTEGER)
+BEGIN 
+    Call sa_set_http_header('Access-Control-Allow-Origin', '*');
+    select com.idCom, nom, prenom, photo, portrait, cote
+    from dba.communaute as com 
+    
+END
 
 
 /******************************************************/
