5b89a8222113065138e4e8fbb64dde01b81c16b8
noellekh <63249943+noellekh@users.noreply.github.com>
2020-05-10 18:08:16 +0200

    Update noelle_khazoum_webservice_serv_aff.sql

--- backendProjet/scripts-api-rest/noelle_khazoum_webservice_serv_aff.sql
+++ backendProjet/scripts-api-rest/noelle_khazoum_webservice_serv_aff.sql
@@ -1,13 +1,13 @@
 // Utilise la base de donnée pour affiner les tâches dans la page Affiner et affiche également les tâches dans la page Tâches
 ALTER PROCEDURE "DBA"."proc_aff" ()
 
-RESULT (nom VARCHAR(50),prenom VARCHAR(50),mail VARCHAR (60), telephone VARCHAR (15), ville VARCHAR(50), tache long VARCHAR, details LONG VARCHAR)
+RESULT (nom VARCHAR(50),prenom VARCHAR(50),mail VARCHAR (60), telephone VARCHAR (15), ville VARCHAR(50), tache long VARCHAR, details LONG VARCHAR, categorie VARCHAR (30))
 
 BEGIN
 
    call sa_set_http_header('Access-Control-Allow-Origin', '*');
 
-   select nom, prenom, mail, telephone, ville, tache, details
+   select nom, prenom, mail, telephone, ville, tache, details, categorie
 
    from dba.communaute as C
 
@@ -15,7 +15,7 @@ BEGIN
 
    join dba.villes as V on C.postal = V.postal
    
-   group by C.nom, C.prenom, C.mail, C.telephone, V.ville, T.tache, T.details
+   group by C.nom, C.prenom, C.mail, C.telephone, V.ville, T.tache, T.details, T.categorie
 
 END
 
