f654bd4bac1b833ee0cbe0df62344258184b5c9d
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-10 18:15:47 +0200

    Update prenom_nom-fonctions.sql

--- backendProjet/scripts-api-rest/prenom_nom-fonctions.sql
+++ backendProjet/scripts-api-rest/prenom_nom-fonctions.sql
@@ -1,1 +1,10 @@
-//à faire
+CREATE FUNCTION "DBA"."fct_informations"(in identifiant VARCHAR(16))
+RETURNS varchar(50)
+BEGIN 
+    DECLARE ville_finale varchar(50) ;
+    SET ville_finale =(select ville 
+                      from dba.communaute as com
+                      natural join dba.villes 
+                      where com.idCom = identifiant) ;
+    return ville_finale ;
+END;
