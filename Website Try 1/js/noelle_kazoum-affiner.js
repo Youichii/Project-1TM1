1a7ac649d2b3349219f58f1df1ee7eee4956ac4a
noellekh <63249943+noellekh@users.noreply.github.com>
2020-05-10 17:41:46 +0200

    Update noelle_kazoum-affiner.js

--- Website Try 1/js/noelle_kazoum-affiner.js
+++ Website Try 1/js/noelle_kazoum-affiner.js
@@ -1,90 +1,191 @@
 "use strict";
-// auteur : Noelle Kazoum matricule
+
+// auteur : Noelle Kazoum HE201903
+
+
 
 /* ****************************** */
 
+
+
 // AFFICHER LES TACHES ET LES  INFORMATIONS
+
 // Prendre les informations de la base de donnée et les metre dans le HTML dans un certain ordre
 
+
+
 function affichageTache(){
-    let xhr = new newXMLHttpRequest();
-    xhr.open('get', "http://localhost/serv_donnees", true);
+
+    let xhr = new  XMLHttpRequest();
+
+    xhr.open('get', "http://localhost/serv_aff", true);
+
     xhr.onreadystatechange =
+
         function (){
+
             if (xhr.status == 200 && xhr.readyState == 4){
+
                 description_tache(xhr.responseText)
+
             }
+
         }
+
     xhr.send();
+
 }
+
+
+
 
 
 function description_tache(info){
-    data = JSON.parse(info);
-    let nom_, prenom_, email_, tel_, tache_, descri_, ville_;
-    for (data[0].tache){
-        document.getElementById("idName").innerHTML = data [0].nom ;
-        document.getElementById("iidPren").innerHTML = data[0].prenom;
-        document.getElementById("idEmail").innerHTML = data[0].email;
-        document.getElementById("idVille").innerHTML =data[0].ville ;
+
+    let data = JSON.parse(info);
+	
+	
+
+    
+
+    for (let i in data){
+		
+		//document.getElementById("tacheSummary").innerHTML += '<ul>' + '<li>' + data[0].tache + '</li>' + '</ul>';
+		
+		//document.getElementById("idDescription").innerHTML += data[0].details;
+
+        document.getElementById("idName").innerHTML += data[0].nom ;
+
+        document.getElementById("iidPren").innerHTML += data[0].prenom;
+
+        document.getElementById("idEmail").innerHTML += data[0].mail;
+
+        document.getElementById("idVille").innerHTML += data[0].ville ;
+
     }
+	
+	
+	
+
+
+
+
 
 
 
 }
+
+
+
+
 
 
 
 //AJOUTER UNE TACHE
+
 // Partir du script, afficher dans le HTML et sauvegarder ensuite dans la base de donnée
 
-function AjouterUneNouvelleTache(){
-    let xhr = new XMLHttpRequest();
-    xhr.open('get', "hettp://localhost/serv_donnees")
-}
 
 
 
-function addTask(t){
-    let nouvelleTache;
-    if (t.a_tache.checked == "maison"){
-        nouvelleTache = document.getElementsById("maison").value;
+
+
+function addTask(){
+
+    let nouvelleTache, descriptionTache, categorieTache ;
+	let nom_cat = document.getElementsByTagName("a_tache");
+
+    if (nom_cat.checked == "maison"){
+
+        categorieTache = "maison";
+		
+
     }
 
-    if (t.a_tache.checked == "jardin"){
-        nouvelleTache = document.getElementsById("jardin").value;
+
+
+    else if (nom_cat.checked == "jardin"){
+
+        categorieTache = "jardin";
+		
+
     }
 
-    if (t.a_tache.checked == "garage"){
-        nouvelleTache = document.getElementsById("garage").value;
+
+
+    else if (nom_cat.checked == "garage"){
+
+        categorieTache = "garage";
+
     }
 
-    if (t.a_tache.checked == "autre"){
-        nouvelleTache = document.getElementsById("autre").value;
+
+
+    else if (nom_cat.checked == "autre"){
+
+        categorieTache = "autre";
+
     }
+	
+	
+	else if (nom_cat.checked == "reparation"){
+		
+		categorieTache ="reparation";
+	}
+	
+	
+	nouvelleTache = document.getElementById("titre").value;
+	
+	descriptionTache = document.getElementById("champDescri").value;
+		
+		
+		
+
+
 
 
 
     let xhr = new XMLHttpRequest;
-    xhr.open('get', "http://localhost/serv_ajouerTask?task_="+nouvelleTache+"&personne_="+data[0].idCom+"desc_"+data[0].details+"&cat_"+data[0].categorie, true);
+
+    xhr.open('get', "http://localhost/serv_ajouerTask?task_="+nouvelleTache+"&personne_="+document.body.id+"&desc_="+descriptionTache+"&cat_="+categorieTache, true);
+
     xhr.send();
+	
+	
+	//console.log("Votre tâche a bien été ajoutée !" );
+
 }
 
+
+
 //AFFINER LES TACHES
+
 // Prendre les taches existantes à partir de la base de donnée et les trier dans le HTML
 
 
-function affinerTache(){
-    let xhr = newXMLHttpRequest();
-    xhr.open('get', "http://localhost/serv_aff", true);
-    // je dois chercher la categorie dans la table tache)
-    xhr.onreadystatechange =
-        function (){
+
+
+
+function affinerTache(informations){
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('get', "http://localhost/serv_affiner/", true);
+
+    
+
+    xhr.onreadystatechange = function (){
+
             if (xhr.status == 200 && xhr.readyState == 4){
+
                 affiner(xhr.responseText);
 
+
+
             }
-        }
+
+    }
+
     xhr.send();
 
 
@@ -92,68 +193,155 @@ function affinerTache(){
 
 
 
-function affiner(txt){
 
-    let jrep = JSON.parse(txt);
-    let dom
+
+function  affiner(txt){
+
+
+
+     let jrep = JSON.parse(txt);
+	 
+
+   
+
+
 
     for(let i = 0; i =jrep.length; i++){
-        if (jrep [i][categorie] == "maison"){
-            dom = document.getElementsById("idMaison").innerHTML += jrep [i].tache; // += à voir
+
+        if (jrep [i] == "maison"){
+
+             document.getElementsById("idMaison").innerHTML += '<ul>'+ '<li>'+'<a href ="#">'+ jrep [i].tache + '</a>'+ '</li>' + '</ul>'; 
+
+
+
+        }
+		
+
+
+
+        else if (jrep[i] == "jardin"){
+
+             document.getElementsById("idJardin").innerHTML +='<ul>'+ '<li>'+'<a href ="#">'+ jrep [i].tache + '</a>'+ '</li>' + '</ul>';
+
+
 
         }
 
-        if (jrep[i][categorie]== "jardin"){
-            dom = document.getElementsById("idJardin").innerHTML += jrep[i].tache;
+
+
+        else if (jrep[i]== "garage"){
+
+             document.getElementsById("idGarage").innerHTML += '<ul>'+ '<li>'+'<a href ="#">'+ jrep [i].tache + '</a>'+ '</li>' + '</ul>';
 
         }
 
-        if (jrep[i][categorie]== "garage"{
-            dom = document.getElementsById("idGarage").innerHTML += jrep[i].tache;
+        else if (jrep[i] == "autre"){
+
+             document.getElementsById("idAutre").innerHTML += '<ul>'+ '<li>'+'<a href ="#">'+ jrep [i].tache + '</a>'+ '</li>' + '</ul>';
+
         }
-        else {
-            dom = document.getElementsById("idAutre").innerHTML += jrep[i].tache;
-        }
-    }
+		
+		
+		else if (jrep[i] == "reparation"){
+			document.getElementById("idReparation").innerHTML += '<ul>'+ '<li>'+'<a href ="#">'+ jrep [i].tache + '</a>'+ '</li>' + '</ul>';
+		}	
+	}	
+	
+	let var1 = '<ul>';
+	let var2 = '<ul>';
+		
+	for (let i in jrep){
+		
+		
+		
+	    document.getElementById("annonceur").innerHTML += var1 +'<li>'+ '<a href = ProfilPublicPrive.js>' + jrep[i].nom +'</a>'+ '</li>' + '</ul>'; 
+		
+
+    
+		document.getElementById("localisationId").innerHTML += var2 = '<li>' +  jrep[i].ville + '</li>' + '</ul>';
+	}
 
 
-    return dom;
+
+
+
+    
+
 }
+
+
 
 // Barre de recherche
 
+/*
+
 function research(){
+
     let xhr = newXMLHttpRequest();
+
     xhr.open('get', "http://localhost/serv_aff", true);
-    // je dois chercher la categorie dans la table tache)
+
+   
+
     xhr.onreadystatechange =
+
         function (){
+
             if (xhr.status == 200 && xhr.readyState == 4){
+
                 rechercherT(xhr.responseText);
 
+
+
             }
+
         }
+
     xhr.send();
 
+
+
 }
+
+
 
 // à terminer
 
+
+
 function rechercherT(motclé){
+
     let mC = JSON.parse(motclé);
+
     let trouve;
+
     for (i=0; i= mC.lenght; i++){
+
         if (document.getElementById(barrech).value == mC[i].prenom){
+
             // envoyer sur le profil public de la personne
 
+
+
         }
+
+
 
         if(document.getElementById(barrech).value == mC[i].nom){
+
             // envoyer sur le profil public de la personne
+
         }
 
+
+
         if(document.getElementById(barrech).value == mC[i].tache){
+
             if (mC[i].tache == )
+
         }
+
     }
+
 }
+*/
