dab42cfcba89327623fbd9c2159b7c6a3c9e5595
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2020-05-11 12:23:14 +0200

    Update cecile_bonnet-webservice-serv_valeurs.sql

--- backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_valeurs.sql
+++ backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_valeurs.sql
@@ -5,11 +5,11 @@ CREATE PROCEDURE "DBA"."proc_valeurs"()
 RESULT (idCom VARCHAR, nom VARCHAR, prenom VARCHAR, sexe CHAR(1), telephone VARCHAR, mail VARCHAR, anniversaire DATE, photo VARCHAR, portrait VARCHAR, tache VARCHAR, commentaire VARCHAR, auteur VARCHAR, cote VARCHAR, ville VARCHAR)
 BEGIN 
     Call sa_set_http_header('Access-Control-Allow-Origin', '*');
-    select commu.idCom, nom, prenom, sexe, telephone, mail, anniversaire, photo, portrait, tache, commentaire, auteur, cote, ville
+    select commu.idCom, nom, prenom, sexe, telephone, mail, anniversaire, photo, portrait, tache, commentaire, auteur, cote, fct_informations(usager)
     from dba.communaute as commu
     left join dba.taches as tach on commu.idCom = tach.idCom
     left join dba.commentaires as comme on commu.idCom = comme.idCom
-    left join dba.villes as vill on commu.postal = vill.postal 
+    
 END
 
 
