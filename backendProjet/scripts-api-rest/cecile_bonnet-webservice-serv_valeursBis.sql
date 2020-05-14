4784310a2be40bc2db46266506f5b3fcef5c9142
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2020-05-14 15:18:42 +0200

    Update cecile_bonnet-webservice-serv_valeursBis.sql

--- backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_valeursBis.sql
+++ backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_valeursBis.sql
@@ -2,11 +2,11 @@
 
 
 CREATE PROCEDURE "DBA"."proc_valeursBis"(in usager VARCHAR(16)) 
-RESULT (idCom VARCHAR, nom VARCHAR, prenom VARCHAR, sexe CHAR(1), telephone VARCHAR, mail VARCHAR, anniversaire DATE, photo VARCHAR, portrait VARCHAR, 
-        tache VARCHAR, commentaire VARCHAR, auteur VARCHAR, cote VARCHAR, ville VARCHAR)
+RESULT (idCom VARCHAR(16), nom VARCHAR(30), prenom VARCHAR(30), sexe CHAR(1), telephone VARCHAR(15), mail VARCHAR(40), anniversaire DATE, photo VARCHAR(25), portrait VARCHAR(120), 
+        titre_tache VARCHAR(100), commentaire LONG VARCHAR, auteur VARCHAR(16), cote INTEGER, ville VARCHAR(50))
 BEGIN 
     Call sa_set_http_header('Access-Control-Allow-Origin', '*');
-    select commu.idCom, nom, prenom, sexe, telephone, mail, anniversaire, photo, portrait, tache, commentaire, auteur, cote, fct_informations(usager)
+    select commu.idCom, nom, prenom, sexe, telephone, mail, anniversaire, photo, portrait, titre_tache, commentaire, auteur, cote, fct_informations(usager)
     from dba.communaute as commu
     left join dba.taches as tach on commu.idCom = tach.idCom
     left join dba.commentaires as comme on commu.idCom = comme.idCom
