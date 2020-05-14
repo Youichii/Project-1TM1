350fcb8bb2bf28702870a731a650c709038b2111
Youichii <63249434+Youichii@users.noreply.github.com>
2020-05-14 15:51:14 +0200

    Update aurelien_brille-webservice-receiveData.sql

--- backendProjet/scripts-api-rest/aurelien_brille-webservice-receiveData.sql
+++ backendProjet/scripts-api-rest/aurelien_brille-webservice-receiveData.sql
@@ -1,7 +1,7 @@
 /* Auteur : Aurélien Brille HE201788*/
 
 
-CREATE PROCEDURE "DBA"."inscription"(IN LastName varchar(30), IN FirstName VARCHAR(30), IN Username varchar(16), IN uMail varchar(40), IN uPhone varchar(15), IN uVille INTEGER, in uBirth DATE , in uPswd varchar(20), in uSex char(1), in Color varchar(120))
+CREATE PROCEDURE "DBA"."inscription"(IN LastName varchar(30), IN FirstName VARCHAR(30), IN Username varchar(16), IN uMail varchar(40), IN uPhone varchar(15), IN uVille INTEGER, in uBirth DATE , in uPswd long varchar, in uSex char(1), in Color varchar(120))
 BEGIN 
     call sa_set_http_header('Access-Control-Allow-Origin', '*');
     INSERT INTO dba.communaute (idCom, mdp, nom, prenom , sexe, telephone, mail, anniversaire, photo, postal)
