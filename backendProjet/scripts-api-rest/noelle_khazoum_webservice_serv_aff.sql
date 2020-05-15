76b63c588a136d2832a567a8ff6346d5d9b59450
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-15 18:05:09 +0200

    Update noelle_khazoum_webservice_serv_aff.sql

--- backendProjet/scripts-api-rest/noelle_khazoum_webservice_serv_aff.sql
+++ backendProjet/scripts-api-rest/noelle_khazoum_webservice_serv_aff.sql
@@ -1,15 +1,15 @@
 /* Auteur : Noelle Khazoum HE201903 */
 
 CREATE PROCEDURE "DBA"."proc_aff" ()
-RESULT (nom VARCHAR(30),prenom VARCHAR(30),mail VARCHAR (40), telephone VARCHAR (15), ville VARCHAR(50), titre_tache VARCHAR(100), desc_tache  VARCHAR, categorie VARCHAR (30))
+RESULT (idCom VARCHAR(16),nom VARCHAR(30),prenom VARCHAR(30),mail VARCHAR (40), telephone VARCHAR (15), ville VARCHAR(50), titre_tache VARCHAR(100), desc_tache  VARCHAR, categorie VARCHAR (30))
 BEGIN
    call sa_set_http_header('Access-Control-Allow-Origin', '*');
-   select nom, prenom, mail, telephone, ville, titre_tache, desc_tache, categorie
+   select C.idCom, nom, prenom, mail, telephone, ville, titre_tache, desc_tache, categorie
    from dba.communaute as C
    join dba.taches as T on C.idCom = T.idCom
    join dba.villes as V on C.postal = V.postal
    natural join dba.categories as cat on T.idCat = cat.idCat
-   group by C.nom, C.prenom, C.mail, C.telephone, V.ville, T.titre_tache, T.desc_tache, cat.categorie
+   group by c.idCom, C.nom, C.prenom, C.mail, C.telephone, V.ville, T.titre_tache, T.desc_tache, cat.categorie
    order by cat.categorie 
 END
 
