0e8e5fa33233b730c99408f694abbcefe1edd56a
noellekh <63249943+noellekh@users.noreply.github.com>
2020-05-15 16:06:49 +0200

    Update noelle_khazoum_webservice_serv_aff.sql

--- backendProjet/scripts-api-rest/noelle_khazoum_webservice_serv_aff.sql
+++ backendProjet/scripts-api-rest/noelle_khazoum_webservice_serv_aff.sql
@@ -1,13 +1,13 @@
 // Utilise la base de donnée pour affiner les tâches dans la page Affiner et affiche également les tâches dans la page Tâches
-ALTER PROCEDURE "DBA"."proc_aff" ()
+CREATE PROCEDURE "DBA"."proc_aff" ()
 
-RESULT (nom VARCHAR(50),prenom VARCHAR(50),mail VARCHAR (60), telephone VARCHAR (15), ville VARCHAR(50), tache long VARCHAR, details LONG VARCHAR, categorie VARCHAR (30))
+RESULT (nom VARCHAR(30),prenom VARCHAR(30),mail VARCHAR (40), telephone VARCHAR (15), ville VARCHAR(50), titre_tache VARCHAR(100), desc_tache  VARCHAR, categorie VARCHAR (30))
 
 BEGIN
 
    call sa_set_http_header('Access-Control-Allow-Origin', '*');
 
-   select nom, prenom, mail, telephone, ville, tache, details, categorie
+   select nom, prenom, mail, telephone, ville, titre_tache, desc_tache, categorie
 
    from dba.communaute as C
 
@@ -15,8 +15,13 @@ BEGIN
 
    join dba.villes as V on C.postal = V.postal
    
-   group by C.nom, C.prenom, C.mail, C.telephone, V.ville, T.tache, T.details, T.categorie
-   order by T.categorie
+   natural join dba.categories as cat on T.idCat = cat.idCat
+
+   
+
+ group by C.nom, C.prenom, C.mail, C.telephone, V.ville, T.titre_tache, T.desc_tache, cat.categorie
+
+ order by cat.categorie 
 
 END
 
