e627d1788930c0de7d5033a5edd544e638ec6ea4
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-15 16:15:14 +0200

    Update noelle_khazoum_webservice_serv_ajouterTask.sql

--- backendProjet/scripts-api-rest/noelle_khazoum_webservice_serv_ajouterTask.sql
+++ backendProjet/scripts-api-rest/noelle_khazoum_webservice_serv_ajouterTask.sql
@@ -1,13 +1,15 @@
-// pour ajouter des taches à la base de donnée
+/* Auteur : Noelle Khazoum HE201903 */
 
 CREATE PROCEDURE "DBA"."proc_ajouterTask"(in titreTask_ varchar(100), task_ integer, in personne_ integer, in desc_ long varchar, in cat_ varchar(30))
 BEGIN 
-     
      insert into dba.taches
      (idCom, idTaches, desc_tache, idCat, titre_tache)
      VALUES 
      (task_,personne_,desc_,cat_,titreTask_);
 END
+
+
+/******************************************************/
 
 
 CREATE SERVICE "serv_ajouterTask" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_ajouterTask(:task_,:personne_,:desc_,:cat_);
