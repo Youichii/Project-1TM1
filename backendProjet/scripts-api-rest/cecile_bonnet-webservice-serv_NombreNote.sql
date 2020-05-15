39941a5e7529a437efa6a27aba940580c46789e6
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2020-05-15 12:38:05 +0200

    Create cecile_bonnet-webservice-serv_NombreNote.sql

---
+++ backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_NombreNote.sql
@@ -0,0 +1,12 @@
+/* Auteur : Cécile Bonnet HE201796 */
+
+CREATE  PROCEDURE proc_NombreNote(in new_utilisateur VARCHAR(16))
+BEGIN 
+update dba.communaute 
+set nombreCote = nombre_cote + 1
+where idCom = new_utilisateur 
+END
+
+/******************************************************/
+
+CREATE SERVICE "serv_NombreNote" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_NombreNote(:new_utilisateur);
