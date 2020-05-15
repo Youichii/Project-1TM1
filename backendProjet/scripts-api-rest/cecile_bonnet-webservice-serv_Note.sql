d0d18bf2f3e4047ae23622b7423a89524ad23073
Clémentine <63249463+sacreclementine@users.noreply.github.com>
2020-05-15 17:00:13 +0200

    Update cecile_bonnet-webservice-serv_Note.sql

--- backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_Note.sql
+++ backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_Note.sql
@@ -1,10 +1,10 @@
 /* Auteur : Cécile Bonnet HE201796 */
 
 
-CREATE PROCEDURE proc_Note(in new_note INTEGER,new_utilisateur VARCHAR(12))
+CREATE PROCEDURE "DBA"."proc_Note"(in new_note INTEGER, new_utilisateur VARCHAR(16))
 BEGIN 
-  update dba.communautee
-  set note  = new_note, nombreCote = nombreCote + 1 
+  update dba.communaute
+  set cote  = new_note, nombreCote = nombreCote + 1 
   where idCom = new_utilisateur 
 END
 
@@ -12,4 +12,4 @@ END
 /******************************************************/
 
 
-CREATE SERVICE "serv_Note" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_Note(:new_note,:utilisateur);
+CREATE SERVICE "serv_Note" TYPE 'JSON' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call proc_Note(:new_note,:new_utilisateur);
