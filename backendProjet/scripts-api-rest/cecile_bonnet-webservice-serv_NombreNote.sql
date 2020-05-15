17a4fe31e472bfc69621c0aedece572cab74902e
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2020-05-15 12:50:20 +0200

    Update cecile_bonnet-webservice-serv_NombreNote.sql

--- backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_NombreNote.sql
+++ backendProjet/scripts-api-rest/cecile_bonnet-webservice-serv_NombreNote.sql
@@ -1,11 +1,13 @@
 /* Auteur : Cécile Bonnet HE201796 */
 
+
 CREATE  PROCEDURE proc_NombreNote(in new_utilisateur VARCHAR(16))
 BEGIN 
   update dba.communaute 
   set nombreCote = nombre_cote + 1
   where idCom = new_utilisateur 
 END
+                                                             
 
 /******************************************************/
 
