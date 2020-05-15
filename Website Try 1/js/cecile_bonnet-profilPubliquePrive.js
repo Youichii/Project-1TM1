e8b4fe2284a66fa3c0c18b4e6d6dfef2b20417e8
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2020-05-15 15:32:50 +0200

    Update cecile_bonnet-profilPubliquePrive.js

--- Website Try 1/js/cecile_bonnet-profilPubliquePrive.js
+++ Website Try 1/js/cecile_bonnet-profilPubliquePrive.js
@@ -29,15 +29,16 @@ function identifiant(){
 		}
 	}
 	xhr.send() ; 
-console.log(id)	
+
 }
  
 
 function infos(donnee) {
     reponse = JSON.parse(donnee); 
 	
-    let nom, prenom, sexe, telephone, mail,anniversaire, photo, portrait, tache, commentaire, auteur, cote, ville ;
+    let nom, prenom, sexe, telephone, mail,anniversaire, photo, portrait, tache, commentaire, auteur, cote, ville, nombreCote ;
     let comment = "" ;
+	nombreCote = reponse[0].nombreCote
 	nom = reponse[0].nom ;
 	prenom= reponse[0].prenom ;
 	sexe = reponse[0].sexe ;
@@ -75,16 +76,16 @@ function infos(donnee) {
 	tache = reponse[0].tache ;
 	if(tache == null){
 		tache = "Pas de tâche pour le moment" ;
-		console.log(tache) ;
+		
 	}
 	else{
 		tache = reponse[0].tache ;
-		console.log(tache) ;
+		
 	}
 	
 	ville = reponse[0].ville ;
     document.getElementById("page").innerHTML = prenom + ' ' + nom + ' ' + '<br>' + sexe + '<br>' + ville + '<br>' + telephone + '<br>' + mail + '<br>' + anniversaire + '<br>' + portrait ;
-    document.getElementById("notation").innerHTML = cote ;
+    document.getElementById("notation").innerHTML = cote/ nombreCote ;
     document.getElementById("zonecomm").innerHTML = comment ;
     document.getElementById("tache").innerHTML = tache ;
 	document.getElementById("mon_img").innerHTML =  '<img src="img?url='+ photo +'" alt="test" id="photo_img" width="234" height="250"/>' ;	
@@ -92,19 +93,24 @@ function infos(donnee) {
 
 
 function note(){
-	let inter = Number(reponse[0].cote)
-	let newN = +prompt("Veuillez introduire une note sur 10") ;
-	let newNote
-	 
-	if(newN <= 10 && newN > 0 && newN != String){
-		if(inter === null ){
+	let totalNote = Number(reponse[0].cote)
+	let addNote = +prompt("Veuillez introduire une note sur 10") ;
+	let newNote, new_N;
+	
+	if(addNote <= 10 && addNote > 0 && addNote != String){
+		if(totalNote === null ){
 			newNote = 0
-			newNote= newN
+			newNote= addNote
+			new_N = addNote
 		}
         else{
 			newNote = 0
-			newNote += (newN + inter)/2 ;
+			new_N = 0 
+			newNote += (totalNote + addNote)
 			console.log(newNote)
+			new_N += newNote/((reponse[0].nombreCote)+1) ;
+			console.log(new_N)
+			
 			alert("votre cote à bien été enregistré")
 		}		
 	}
@@ -113,10 +119,15 @@ function note(){
 	}
 	let xhr = new XMLHttpRequest() ;  
 	xhr.open('get', "http://localhost/serv_Note?new_note="+newNote+"&utilisateur="+reponse[0].idCom, true) ;
-	console.log(reponse[0].idCom)	
-	console.log("coucou")
+	
+	
 	xhr.send() ;
-	document.getElementById("notation").innerHTML = newNote
+	// xhr.open('get', "http://localhost/serv_NombreNote?utilisateur="+reponse[0].idCom, true) ;
+	
+	
+	// xhr.send() ;
+	
+	document.getElementById("notation").innerHTML = new_N;
 }
 	
 function commentaires(){
@@ -139,13 +150,13 @@ function pageChargementBis(user){
 		}
 	}
 	xhr.send() ; 	
-	console.log(user)
+	
 }
 
 
 function informations(donnee) {
     info = JSON.parse(donnee); 
-	console.log(info) ;
+	
     let nom, prenom, sexe, telephone, mail,anniversaire, photo, portrait, tache, ville ;
 	ville=info[0].ville
 	nom =info[0].nom
@@ -154,7 +165,7 @@ function informations(donnee) {
 	telephone = info[0].telephone ;
 	mail = info[0].mail ;
 	anniversaire = info[0].anniversaire ;
-	console.log(anniversaire)
+
 	photo = info[0].photo
 	
 	portrait=info[0].portrait
@@ -185,15 +196,15 @@ function informations(donnee) {
 			            + ' <input type="button" onclick="changVille()" value="Changer la ville" id="Ville" class="change">'
 	
 	document.getElementById("services").innerHTML= tache;
-    console.log(tache)
+   
     document.getElementById("description").innerHTML = portrait
-    console.log(portrait)
+    
     document.getElementById("cadrephoto").innerHTML =  '<img src="img?url='+ photo +'" alt="test" width="234" height="250"/>' ;
 }
 
 function changTelephone(){
 	let newTelephone = prompt("Introduire votre nouveau numéro de telephone");
-	console.log(newTelephone)
+	
 	let xhr = new XMLHttpRequest() ;  
 	xhr.open('get', "http://localhost/serv_Telephone?new_telephone="+newTelephone+ "&new_utilisateur="+document.body.id, true) ;
 	
@@ -217,7 +228,7 @@ function changMail(){
 
 function changVille(){
 	let newVille = document.getElementById("viLLe").value
-	console.log(newVille + "  bonjour")
+	
 	if( newVille == 1000	) {document.getElementById("ville").innerHTML = 'Bruxelles' +"   <select  name='villes' id='viLLe' class='change'><option value='1000'>1000 - Bruxelles</option><option value='1348' selected>1348 - Louvain-la-Neuve</option><option value='2000'>2000 - Anvers</option><option value='3500'>3500 - Hasselt</option><option value='4000'>4000 - Liège</option><option value='5000'>5000 - Namur</option><option value='6600'>6600 - Bastogne</option></select>" +
 	                                            ' <input type="button" onclick="changVille()" value="Changer la ville" id="ville" class="change">'}
 	else if( newVille == 1348 ) {document.getElementById("ville").innerHTML = 'Louvain-La-Neuve' +"   <select  name='villes' id='viLLe' class='change'><option value='1000'>1000 - Bruxelles</option><option value='1348' selected>1348 - Louvain-la-Neuve</option><option value='2000'>2000 - Anvers</option><option value='3500'>3500 - Hasselt</option><option value='4000'>4000 - Liège</option><option value='5000'>5000 - Namur</option><option value='6600'>6600 - Bastogne</option></select>" +
@@ -241,7 +252,7 @@ function changVille(){
 
 function changDescription(){ 
 	let newDescription = document.getElementById("descriptionTexte").value ;
-	console.log(newDescription) ;
+	
 	let xhr = new XMLHttpRequest() ;  
 	xhr.open('get', "http://localhost/serv_Desc?new_desc="+newDescription+"&new_utilisateur="+document.body.id, true) ;
 	
